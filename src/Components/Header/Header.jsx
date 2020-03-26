cfc01bd3fb33ebfca018f26df4edb408aec6089e
adilkhan <adilkhan@ucsb.edu>
2020-03-26 15:11:24 -0700

    Added logo and finished cleaning up rest of the code

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { AppBar, Toolbar } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
+import img from '../../Images/logo.png'
 
 const useStyles = makeStyles(theme => ({
     toolBar : {
         ...theme.mixins.toolbar 
+    },
+    img : {
+        width : "80px",
+        height : "80px"
+    },
+    nav : {
+        backgroundColor : "white"
     }
 }))
 
@@ -12,9 +20,9 @@ const Header = (props) =>{
     const classes = useStyles()
     return(
         <>
-            <AppBar>
+            <AppBar className={classes.nav}>
                 <Toolbar>
-
+                    <img src ={img} className={classes.img}/>
                 </Toolbar>
             </AppBar>
             <div className={classes.toolBar} />
