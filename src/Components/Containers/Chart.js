d72d8170c7e9c563bf68e30214723a10ef1089eb
adilkhan <adilkhan@ucsb.edu>
2021-05-21 12:58:46 -0700

    didn't fix anything but it started working again

--- src/Components/Containers/Chart.js
+++ src/Components/Containers/Chart.js
@@ -1,37 +1,26 @@
-import React from 'react'
+import React from 'react';
 import { Bar, Line, Pie, Bubble, Doughnut } from 'react-chartjs-2';
 
-const Chart = (props)=> {
-        const data = {
-            labels: [
-                "Deaths", "Cases"
-            ],
-            datasets: [{
-                label: "Covid-19 stats", 
-                data: [
-                    props.data && props.data[0].deaths ? props.data[0].deaths : 1, 
-                    props.data && props.data[0].cases ? props.data[0].cases : 1, 
-                ],
-                backgroundColor: [
-                '#FF6384',
-                '#36A2EB',
-                '#FFCE56',
-                "red",
-                "teal",
-                "green"
-                ],
-                hoverBackgroundColor: [
-                '#FF6384',
-                '#36A2EB',
-                '#FFCE56'
-                ]
-            }]
-        };
-        return (
-            <div className="charts">
-                <Doughnut data={data} options={{}} />
-            </div>
-        )
-}
+const Chart = (props) => {
+	const data = {
+		labels: [ 'Deaths', 'Cases' ],
+		datasets: [
+			{
+				label: 'Covid-19 stats',
+				data: [
+					props.data && props.data[0].deaths ? props.data[0].deaths : 1,
+					props.data && props.data[0].cases ? props.data[0].cases : 1
+				],
+				backgroundColor: [ '#FF6384', '#36A2EB', '#FFCE56', 'red', 'teal', 'green' ],
+				hoverBackgroundColor: [ '#FF6384', '#36A2EB', '#FFCE56' ]
+			}
+		]
+	};
+	return (
+		<div className="charts">
+			<Doughnut data={data} options={{}} />
+		</div>
+	);
+};
 
-export default Chart
+export default Chart;
